2195a30e0e5e97cb80d5ca7c7617b0558f24f821
Chrysellia <priscilliachryso@gmail.com>
2021-12-07 12:22:30 +0300

    Cart component updated

--- ecommerce-challenge/src/MainPage/Cart/Cart.component.js
+++ ecommerce-challenge/src/MainPage/Cart/Cart.component.js
@@ -3,32 +3,39 @@ import './Cart.style.scss'
 
 const Cart = () => {
     return (
-        <div className="cart-page">
+        <div className="cart-page shadow-2xl">
             <div className="cart-content">
                 <div className="cart-title">Cart</div>
                 <div className="cart-line"></div>
                 <div className="cart-description">
                     <div className="cart-empty">
-                        <div className="text-center">Your cart is empty</div>
+                        <div className="empty-message">Your cart is empty</div>
                     </div>
                     <div className="cart-full">
                         <div className="full-description">
                             <div className="item-image">
                                 <img src="images/image-product-1.jpg"/>
                             </div>
-                            <div className="item-title">Fall Limited Edition Sneakers</div>
-                            <div className="item-price">
-                                <div className="item-amount"></div>
-                                <div className="item-quantity"></div>
-                                <div className="item-result"></div>
-                            </div>
-                            <div className="delete-item">
-                                <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs>
-                                    <path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/>
-                                </svg>
+                            <div className="flex">
+                                <div className="description-element">
+                                    <div className="item-title">Fall Limited Edition Sneakers</div>
+                                    <div className="flex">
+                                        <div className="item-price">
+                                            <div className="price-element">$125.00</div>
+                                            <div className="price-element">x</div>
+                                            <div className="price-element">3</div>
+                                            <div className="price-result text-black">$375.00</div>
+                                        </div>
+                                        <div className="delete-item">
+                                            <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><defs>
+                                                <path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlinkHref="#a"/>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <button className="text-center text-white font-bold py-2 px-4 rounded" style={{backgroundColor:'hsl(26, 100%, 55%)'}}>Checkout</button>
+                        <button className="text-center text-white font-bold py-2 px-14 mb-5 rounded" style={{backgroundColor:'hsl(26, 100%, 55%)'}}>Checkout</button>
                     </div>
                 </div>
             </div>
--- ecommerce-challenge/src/MainPage/Navbar/Navbar.component.js
+++ ecommerce-challenge/src/MainPage/Navbar/Navbar.component.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Navbar.style.scss';
+import Cart from '../Cart/Cart.component';
 
 const Navbar = () => {
+    const [modal, setModal] = React.useState(false);
+
     return (
         <div className="navbar-page">
             <div className="navbar-content">
                 <div className="navbar-menu">
-                    <div className="navbar-item text-3xl" style={{color: 'black', fontWeight:'bold'}}>Sneakers</div>
+                    <div className="navbar-item" style={{color:'black', fontWeight:'bolder'}}>Sneakers</div>
                     <div className="navbar-item">Collections</div>
                     <div className="navbar-item">Men</div>
                     <div className="navbar-item">Women</div>
@@ -14,10 +17,11 @@ const Navbar = () => {
                     <div className="navbar-item">Contact</div>
                 </div> 
                 <div className="navbar-object">
-                    <div className="navbar-cart">
+                    <div className="navbar-cart" onClick={()=> setModal(true)}>
                         <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg">
                             <path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#69707D" fill-rule="nonzero"/>
                         </svg>
+                        <div className="cart-number">3</div>
                     </div>
                     <div className="navbar-profile">
                         <img src="images/image-avatar.png"/>
@@ -25,6 +29,8 @@ const Navbar = () => {
                 </div>
             </div>
             <div className="navbar-line"></div>
+
+            { modal ? (<Cart/>) : null}
         </div>
     )
 }
